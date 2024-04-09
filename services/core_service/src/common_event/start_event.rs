ba2a573d0e7b145b510f28aa31cb8fc570a212cb
尹耀德 <yinyaode1@huawei.com>
2024-04-09 17:58:32 +0800

    modify asset to Non-persistent process
    Signed-off-by: 尹耀德 <yinyaode1@huawei.com>
    Change-Id: I391dfd07efbd59af163bd06dfaa542e31e06372f

---
+++ services/core_service/src/common_event/start_event.rs
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! This module is used to handle start event.
+
+use std::collections::HashMap;
+
+use asset_file_operator::delete_user_db_dir;
+use asset_log::{loge, logi};
+use system_ability_fwk::cxx_share::SystemAbilityOnDemandReason;
+
+use crate::common_event::listener;
+
+const USER_ID: &str = "userId";
+const SANDBOX_APP_INDEX: &str = "sandbox_app_index"; 
+const APP_ID: &str = "appId";
+
+fn handle_package_removed(want: &HashMap<String, String>, is_sandbox: bool) {    
+    let Some(user_id) = want.get(USER_ID) else {
+        loge!("[FATIL]Get removed owner info failed, get userId fail");
+        return; 
+    };
+    let Some(app_id) = want.get(APP_ID) else {
+        loge!("[FATIL]Get removed owner info failed, get appId fail");
+        return;
+    };
+
+    let mut app_index = 0;
+    if is_sandbox { 
+        app_index = match want.get(SANDBOX_APP_INDEX) {
+            Some(v) => match v.parse::<i32>() {
+                Ok(parsed_value) => parsed_value,
+                Err(_) => {
+                    loge!("[FATAL]Get removed owner info failed, failed to parse appIndex");
+                    return;
+                }
+            },
+            None => {
+                loge!("[FATIL]Get removed owner info failed, get appIndex fail");
+                return;
+            },
+        }
+    }
+    let owner = format!("{}_{}", app_id, app_index);
+    let user_id = match user_id.parse::<i32>() {
+        Ok(parsed_value) => parsed_value,
+        Err(_) => {
+            loge!("[FATIL]Get removed user_id failed, failed to parse user_id");
+            return;
+        }
+    };
+    listener::delete_data_by_owner(user_id, owner.as_ptr(), owner.len() as u32);
+}
+
+pub(crate)fn handle_common_event(reason: SystemAbilityOnDemandReason) {
+    let reason_name: String = reason.name;
+    if reason_name == "usual.event.PACKAGE_REMOVED" {
+        let want = reason.extra_data.want();
+        handle_package_removed(&want, false);
+    } else if reason_name == "usual.event.SANDBOX_PACKAGE_REMOVED" {
+        let want = reason.extra_data.want();
+        handle_package_removed(&want, true);
+    } else if reason_name == "usual.event.USER_REMOVED" {
+        logi!("on_start by user remove");
+        let _ = delete_user_db_dir(reason.extra_data.code);
+    } else if reason_name == "usual.event.CHARGING" {
+        listener::backup_db();
+    }
+}
--- services/core_service/src/lib.rs
+++ services/core_service/src/lib.rs
@@ -15,49 +15,90 @@
 
 //! This module implements the Asset service.
 
-use std::{thread, time::Instant};
+use std::{time::{Duration, Instant}};
 
-use system_ability_fwk::ability::{Ability, PublishHandler};
+use samgr::manage::SystemAbilityManager;
+use system_ability_fwk::{ability::{Ability, Handler}, cxx_share::SystemAbilityOnDemandReason};
+use ylong_runtime::time::sleep;
 
 use asset_constants::CallingInfo;
+use asset_crypto_manager::crypto_manager::CryptoManager;
 use asset_definition::{log_throw_error, AssetMap, ErrCode, Result};
 use asset_ipc::SA_ID;
 use asset_log::{loge, logi};
 
-mod listener;
+mod common_event;
+mod counter;
 mod operations;
 mod stub;
 mod sys_event;
 mod trace_scope;
+mod unload_handler;
 
 use sys_event::upload_system_event;
 use trace_scope::TraceScope;
 
+use crate::counter::Counter;
+use crate::unload_handler::{UnloadHandler, SEC_TO_MILLISEC, DELAYED_UNLOAD_TIME_IN_SEC};
+
 struct AssetAbility;
 
+pub(crate) fn unload_sa(duration: u64) {
+    let unload_handler = UnloadHandler::get_instance();
+    unload_handler.lock().unwrap().update_task(ylong_runtime::spawn(async move {
+        sleep(Duration::from_secs(duration)).await;
+        SystemAbilityManager::unload_system_ability(SA_ID);
+    }));
+}
+
 impl Ability for AssetAbility {
-    fn on_start(&self, handler: PublishHandler) {
+    fn on_start_with_reason(&self, reason: SystemAbilityOnDemandReason, handler: Handler) {
+        logi!("[INFO]Start asset service, reason_id: {:?}", reason.reason_id);
+        common_event::handle_common_event(reason);
+
         let func_name = hisysevent::function!();
         let start = Instant::now();
         let _trace = TraceScope::trace(func_name);
         let calling_info = CallingInfo::new_self();
 
         let _ = upload_system_event(start_service(handler), &calling_info, start, func_name);
+        unload_sa(DELAYED_UNLOAD_TIME_IN_SEC as u64);
+    }
+
+    fn on_active(&self, reason: SystemAbilityOnDemandReason) {
+        logi!("[INFO]Asset service on_active.");
+        common_event::handle_common_event(reason);
+    }
+
+    fn on_idle(&self, _reason: SystemAbilityOnDemandReason) -> i32 {
+        let crypto_manager = CryptoManager::get_instance();
+        let max_crypto_expire_duration = crypto_manager.lock().unwrap().max_crypto_expire_duration();
+        if max_crypto_expire_duration > 0 {
+            logi!("[INFO]Asset service on idle not success, delay time: {}s", max_crypto_expire_duration);
+            return max_crypto_expire_duration as i32 * SEC_TO_MILLISEC;
+        }
+
+        let counter = Counter::get_instance();
+        if counter.lock().unwrap().count() > 0 {
+            logi!("[INFO]Asset service on idle not success for use_account: {}, delay time: {}s",
+                counter.lock().unwrap().count(), DELAYED_UNLOAD_TIME_IN_SEC);
+            return DELAYED_UNLOAD_TIME_IN_SEC * SEC_TO_MILLISEC;
+        }
+        logi!("[INFO]Asset service on_idle.");
+        0
     }
 
     fn on_stop(&self) {
         logi!("[INFO]Asset service on_stop");
-        listener::unsubscribe();
+        common_event::unsubscribe();
     }
 }
 
-fn start_service(handler: PublishHandler) -> Result<()> {
-    if handler.publish(AssetService) {
+fn start_service(handler: Handler) -> Result<()> {
+    common_event::subscribe();
+    if handler.publish(AssetService::new(handler.clone())) {
         return log_throw_error!(ErrCode::IpcError, "Asset publish stub object failed");
     };
-
-    logi!("[INFO]Asset service on_start");
-    thread::spawn(listener::subscribe);
     Ok(())
 }
 
@@ -75,7 +116,9 @@ static A: extern "C" fn() = {
     init
 };
 
-struct AssetService;
+struct AssetService {
+    system_ability: system_ability_fwk::ability::Handler
+}
 
 macro_rules! execute {
     ($func:path, $($args:expr), *) => {{
@@ -88,6 +131,10 @@ macro_rules! execute {
 }
 
 impl AssetService {
+    pub(crate) fn new(handler: system_ability_fwk::ability::Handler) -> Self {
+        Self { system_ability: handler }
+    }
+
     fn add(&self, attributes: &AssetMap) -> Result<()> {
         execute!(operations::add, attributes)
     }
