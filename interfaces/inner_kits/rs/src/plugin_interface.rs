7b55e94abd1f338828cd24829c69b968d5b833e6
尹耀德 <yinyaode1@huawei.com>
2025-02-11 10:06:50 +0800

    change with commonts
    Signed-off-by: 尹耀德 <yinyaode1@huawei.com>

--- interfaces/inner_kits/rs/src/plugin_interface.rs
+++ interfaces/inner_kits/rs/src/plugin_interface.rs
@@ -18,7 +18,6 @@
 pub use asset_definition::Value;
 use ipc::parcel::MsgParcel;
 use std::any::Any;
-use std::cmp::Ordering;
 use std::collections::HashMap;
 
 /// Defines a type alias `ExtDbMap` as a `HashMap` with keys of type `&'static str` and values of type `Value`.
