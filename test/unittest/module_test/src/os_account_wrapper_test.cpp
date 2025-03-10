e456caaa8d3946638c7a64e15d1ff7850fa9b7d3
尹耀德 <yinyaode1@huawei.com>
2025-03-10 21:57:38 +0800

    tdd change
    Signed-off-by: 尹耀德 <yinyaode1@huawei.com>

--- test/unittest/module_test/src/os_account_wrapper_test.cpp
+++ test/unittest/module_test/src/os_account_wrapper_test.cpp
@@ -98,7 +98,7 @@ HWTEST_F(AssetOsAccountWrapperTest, AssetOsAccountWrapperTest004, TestSize.Level
 {
     int32_t userIds[10];
     uint32_t userIdsSize = 10;
-    ASSERT_EQ(ASSET_SUCCESS, GetUserIds(userIds, &userIdsSize));
+    ASSERT_EQ(0, GetUserIds(userIds, &userIdsSize));
 }
 
 /**
@@ -110,6 +110,6 @@ HWTEST_F(AssetOsAccountWrapperTest, AssetOsAccountWrapperTest004, TestSize.Level
 HWTEST_F(AssetOsAccountWrapperTest, AssetOsAccountWrapperTest005, TestSize.Level0)
 {
     uint32_t userIdsSize = 10;
-    ASSERT_EQ(ASSET_SUCCESS, GetUsersSize(&userIdsSize));
+    ASSERT_EQ(0, GetUsersSize(&userIdsSize));
 }
 }
