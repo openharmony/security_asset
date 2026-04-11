74bea994ab161257482c70cf9f5601e20e97bf0e
Ziyi Zhong <zhongziyi3@huawei.com>
2026-04-11 11:00:06 +0800

    TDD for Batch
    Signed-off-by: Ziyi Zhong <zhongziyi3@huawei.com>

---
+++ test/unittest/module_test/inc/asset_system_batch_api_test.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ASSET_SYSTEM_BATCH_API_TEST_H
+#define ASSET_SYSTEM_BATCH_API_TEST_H
+
+#define NOT_FOUND_USER_ID 103
+
+namespace UnitTest::AssetSystemBatchApiTest {
+int AssetSystemBatchApiTest001(void);
+int AssetSystemBatchApiTest002(void);
+int AssetSystemBatchApiTest003(void);
+int AssetSystemBatchApiTest004(void);
+int AssetSystemBatchApiTest005(void);
+int AssetSystemBatchApiTest006(void);
+int AssetSystemBatchApiTest007(void);
+int AssetSystemBatchApiTest008(void);
+int AssetSystemBatchApiTest009(void);
+}
+
+#endif // ASSET_SYSTEM_BATCH_API_TEST_H
