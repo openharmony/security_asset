cf5a02e2ba541ddb21c038532c8bcd49de9f1d4f
darkness33 <zhongyan27@huawei.com>
2026-06-17 19:08:55 +0800

    asset snapshot change
    Signed-off-by: darkness33 <zhongyan27@huawei.com>

--- interfaces/inner_kits/rs/asset/src/full.rs
+++ interfaces/inner_kits/rs/asset/src/full.rs
@@ -20,10 +20,11 @@ use std::{
     time::Instant,
 };
 
+use std::{ffi::CString, os::raw::c_char, sync::OnceLock};
 pub use asset_definition::*;
 
 use asset_log::logw;
-use ipc::{parcel::MsgParcel, remote::RemoteObj};
+use ipc::{parcel::{MsgParcel, MsgOption}, remote::RemoteObj};
 use samgr::manage::SystemAbilityManager;
 
 pub use asset_ipc::{
@@ -33,12 +34,62 @@ pub use asset_ipc::{
 
 extern "C" {
     fn GetTimeOut(timeout: *mut i32) -> i32;
+    fn IsBeforeImageCreationPoint() -> bool;
+    fn IsAbilityCreated() -> bool;
+    fn ReportSnapshotFailure(load_interface_name: *const c_char, unload_interface_name: *const c_char);
 }
 
 const LOAD_TIMEOUT_IN_SECONDS: i32 = 4;
 const SUCCESS: i32 = 0;
 const MAX_ARRAY_CAPACITY: usize = 100;
 static ASSET_PLUGIN_LOCK: Mutex<()> = Mutex::new(());
+
+struct ImageInfo {
+    use_before_flag: bool,
+    has_notify: bool,
+    calling_func: IpcCode,
+    has_groupid: bool,
+}
+
+impl ImageInfo {
+    fn new() -> Self {
+        ImageInfo { use_before_flag: false, has_notify: false, calling_func: IpcCode::Add,
+            has_groupid: false}
+    }
+
+    /// Get the single instance of Counter.
+    pub fn get_instance() -> Arc<Mutex<ImageInfo>> {
+        static INSTANCE: OnceLock<Arc<Mutex<ImageInfo>>> = OnceLock::new();
+        INSTANCE.get_or_init(|| {
+            Arc::new(Mutex::new(ImageInfo::new()))
+        }).clone()
+    }
+
+    pub(crate) fn set_flag(&mut self, calling_func: IpcCode) {
+        self.use_before_flag = true;
+        self.calling_func = calling_func;
+    }
+
+    pub(crate) fn has_set_flag(&self) -> bool {
+        self.use_before_flag
+    }
+
+    pub(crate) fn get_calling_func(&self) -> IpcCode {
+        self.calling_func
+    }
+
+    pub(crate) fn has_notify(&self) -> bool {
+        self.has_notify
+    }
+
+    pub(crate) fn has_groupid(&self) -> bool {
+        self.has_groupid
+    }
+
+    pub(crate) fn set_has_notify(&mut self) {
+        self.has_notify = true;
+    }
+}
 
 fn load_asset_service() -> Result<RemoteObj> {
     let mut timeout: i32 = 0;
@@ -85,25 +136,37 @@ impl Manager {
 
     /// Add an Asset.
     pub fn add(&mut self, attributes: &AssetMap) -> Result<()> {
+        self.snapshot_check_before_image(attributes, IpcCode::Add);
+        self.snapshot_check_groupid();
         self.process_one_agr_request(attributes, IpcCode::Add)?;
+        self.snapshot_check_after_image(IpcCode::Add);
         Ok(())
     }
 
     /// Add batch assets.
     pub fn batch_add(&mut self, attributes_array: &Vec<AssetMap>) -> Result<Vec<(u32, u32)>> {
+        self.snapshot_check_batch_before_image(attributes_array, IpcCode::BatchAdd);
+        self.snapshot_check_groupid();
         let ret = self.process_one_array_request_with_ret(attributes_array, IpcCode::BatchAdd)?;
+        self.snapshot_check_after_image(IpcCode::BatchAdd);
         Ok(ret)
     }
 
     /// Remove one or more Assets that match a search query.
     pub fn remove(&mut self, query: &AssetMap) -> Result<()> {
+        self.snapshot_check_before_image(query, IpcCode::Remove);
+        self.snapshot_check_groupid();
         self.process_one_agr_request(query, IpcCode::Remove)?;
+        self.snapshot_check_after_image(IpcCode::Remove);
         Ok(())
     }
 
     /// Remove batch assets.
     pub fn batch_remove(&mut self, attributes_array: &Vec<AssetMap>) -> Result<()> {
+        self.snapshot_check_batch_before_image(attributes_array, IpcCode::BatchRemove);
+        self.snapshot_check_groupid();
         self.process_one_array_request(attributes_array, IpcCode::BatchRemove)?;
+        self.snapshot_check_after_image(IpcCode::BatchRemove);
         Ok(())
     }
 
@@ -113,20 +176,28 @@ impl Manager {
         attributes_array: &Vec<AssetMap>,
         attributes_to_update_array: &Vec<AssetMap>
     ) -> Result<Vec<(u32, u32)>> {
+        self.snapshot_check_batch_before_image(attributes_array, IpcCode::BatchUpdate);
+        self.snapshot_check_groupid();
         let ret = self.process_two_array_request_with_ret(
             attributes_array, attributes_to_update_array, IpcCode::BatchUpdate
         )?;
+        self.snapshot_check_after_image(IpcCode::BatchUpdate);
         Ok(ret)
     }
 
     /// Update an Asset that matches a search query.
     pub fn update(&mut self, query: &AssetMap, attributes_to_update: &AssetMap) -> Result<()> {
+        self.snapshot_check_before_image(query, IpcCode::Update);
+        self.snapshot_check_groupid();
         self.process_two_agr_request(query, attributes_to_update, IpcCode::Update)?;
+        self.snapshot_check_after_image(IpcCode::Update);
         Ok(())
     }
 
     /// Preprocessing for querying one or more Assets that require user authentication.
     pub fn pre_query(&mut self, query: &AssetMap) -> Result<Vec<u8>> {
+        self.snapshot_check_before_image(query, IpcCode::PreQuery);
+        self.snapshot_check_groupid();
         let mut reply = self.process_one_agr_request(query, IpcCode::PreQuery)?;
         let res = reply.read::<Vec<u8>>().map_err(ipc_err_handle)?;
         Ok(res)
@@ -134,6 +205,8 @@ impl Manager {
 
     /// Query one or more Assets that match a search query.
     pub fn query(&mut self, query: &AssetMap) -> Result<Vec<AssetMap>> {
+        self.snapshot_check_before_image(query, IpcCode::Query);
+        self.snapshot_check_groupid();
         let mut reply = self.process_one_agr_request(query, IpcCode::Query)?;
         let res = deserialize_maps(&mut reply)?;
         Ok(res)
@@ -310,7 +383,9 @@ impl Manager {
     }
 
     fn send_request(&self, mut parcel: MsgParcel, ipc_code: IpcCode) -> Result<MsgParcel> {
-        let mut reply = self.remote.send_request(ipc_code as u32, &mut parcel).map_err(ipc_err_handle)?;
+        let mut option = MsgOption::new();
+        option.set_image();
+        let mut reply = self.remote.send_request_ext(ipc_code as u32, &mut parcel, option).map_err(ipc_err_handle)?;
         match reply.read::<u32>().map_err(ipc_err_handle)? {
             IPC_SUCCESS => Ok(reply),
             e => {
@@ -322,5 +397,56 @@ impl Manager {
 
     fn descriptor(&self) -> &'static str {
         SA_NAME
+    }
+
+    fn snapshot_check_batch_before_image(&self, attributes_array: &[AssetMap], ipc_code: IpcCode) {
+        if let Some(first_query) = attributes_array.first() {
+            self.snapshot_check_before_image(first_query, ipc_code);
+        }
+    }
+
+    fn snapshot_check_before_image(&self, query: &AssetMap, ipc_code: IpcCode) {
+        unsafe { if !IsBeforeImageCreationPoint() { return; } }
+        let image_info = ImageInfo::get_instance();
+        let mut res = image_info.lock().unwrap();
+        if !res.has_set_flag() {
+            res.set_flag(ipc_code);
+        }
+        if query.get(&Tag::GroupId).is_some() {
+            res.has_groupid = true;
+        }
+    }
+
+    fn snapshot_check_after_image(&self, ipc_code: IpcCode) {
+        let image_info = ImageInfo::get_instance();
+        let mut res = image_info.lock().unwrap();
+        if res.has_notify() || (!res.has_set_flag()) { return; }
+
+        unsafe {
+            if IsAbilityCreated() {
+                let load_func = res.get_calling_func().to_string();
+                let load_cstr = CString::new(load_func).unwrap();
+                let unload_func = ipc_code.to_string();
+                let unload_cstr = CString::new(unload_func).unwrap();
+                ReportSnapshotFailure(load_cstr.as_ptr(), unload_cstr.as_ptr());
+                res.set_has_notify();
+            }
+        }
+    }
+
+    fn snapshot_check_groupid(&self) {
+        let image_info = ImageInfo::get_instance();
+        let mut res = image_info.lock().unwrap();
+        if res.has_notify() || (!res.has_groupid()) { return; }
+        unsafe {
+            if IsAbilityCreated() {
+                let load_func = res.get_calling_func().to_string();
+                let load_cstr = CString::new(load_func).unwrap();
+                let unload_func = "hasGroupId";
+                let unload_cstr = CString::new(unload_func).unwrap();
+                ReportSnapshotFailure(load_cstr.as_ptr(), unload_cstr.as_ptr());
+                res.set_has_notify();
+            }
+        }
     }
 }
